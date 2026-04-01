a2fd85e170003db9c4b9ba201e49651102240186
Vikas Parmar <95103544+vikasxci@users.noreply.github.com>
2026-04-01 08:18:18 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const BACKEND_URL = 'https://mailingo-81ti.onrender.com/'; // Change this when deploying to a different server
+
+const CONFIG = {
+  backendUrl: BACKEND_URL,
+  apiBase: `${BACKEND_URL}/api/mail`,
+  endpoints: {
+    send: `${BACKEND_URL}/api/mail/send`,
+    history: `${BACKEND_URL}/api/mail/history`,
+  },
+};
